33b025d7fd1991e6aafa4aba2b843908f6125fb0
Dream <sdeepicivil@gmail.com>
2022-11-22 22:40:59 +0530

    ECommerce Task 9

--- views/prac.js
+++
@@ -1,8 +0,0 @@
-function closeCart(){
-   const sect = document.getElementById('cart');
-   sect.style.display='none';
-}
-function showCart(){
-    const sect = document.getElementById('cart');
-    sect.style.display='flex';
- }
---
+++ views/shop.js
@@ -0,0 +1,79 @@
+function closeCart(){
+   const sect = document.getElementById('cart');
+   sect.style.display='none';
+}
+function showCart(){
+    const sect = document.getElementById('cart');
+    sect.style.display='flex';
+ }
+ function addToCart(event){
+   const albumId = event.target.parentNode.parentNode.id;
+   const prodDetail = event.target.parentNode;
+   const price = prodDetail.firstElementChild.firstElementChild.textContent;
+   const imgDetail = prodDetail.previousElementSibling;
+   const img = imgDetail.firstElementChild.src;
+   const name = imgDetail.previousElementSibling.textContent;
+   addCartDetail(albumId,name,img,price);
+ }
+
+ function addCartDetail(id,name,img,price,totalprice){
+   const cartItem = document.getElementById('cart-items');
+
+   const div = document.createElement('div');
+   div.className='cart-row';
+   div.id = `in-cart-${id}`;
+
+   const imgSpan = document.createElement('span');
+   imgSpan.className='cart-item cart-column';
+
+   const image = document.createElement('img');
+   image.className='cart-img';
+   image.src=img;
+   imgSpan.appendChild(image);
+   const headSpan = document.createElement('span');
+   headSpan.textContent=name;
+   imgSpan.appendChild(headSpan);
+
+   div.appendChild(imgSpan);
+
+   const priceSpan = document.createElement('span');
+   priceSpan.className='cart-price cart-column';
+   priceSpan.textContent=price;
+   div.appendChild(priceSpan);
+
+   const quanSpan = document.createElement('span');
+   quanSpan.className='cart-quantity cart-column';
+   const input = document.createElement('input');
+   input.className = 'item-count';
+   input.type='text';
+   input.value='1';
+   quanSpan.appendChild(input);
+   const button = document.createElement('button');
+   button.textContent='REMOVE';
+   quanSpan.appendChild(button);
+   div.appendChild(quanSpan);
+
+   cartItem.appendChild(div);
+
+  const totalVal = document.getElementById('total-value');
+  totalVal.textContent = +totalVal.textContent + +price;
+
+  const totalNo = document.getElementsByClassName('item-count');
+  let count=0;
+  for(let i of totalNo)
+   count += +i.value;
+  const cartVal = document.getElementById('cart-number');
+  cartVal.textContent = count;
+  showNotification(name);
+ }
+
+ function showNotification(name){
+ const notif = document.createElement('div');
+ notif.innerHTML=`<h4>${name} is added to Cart</h4>`;
+ notif.id = 'notification';
+ const notifcontainer = document.getElementById('item-added');
+ notifcontainer.appendChild(notif);
+ setTimeout(()=>{
+  notif.remove();
+ },2000);
+ }
